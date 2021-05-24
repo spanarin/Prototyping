9642b780796fe0fd4982670041e27681b321369d
Sergey Panarin <contact@smartykit.org>
2021-05-24 08:46:50 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Example2 from "./Example2";
+
+export default () => (
+  <div>
+    <div style={{ margin: 20, border: "1px solid gray", padding: 10 }}>
+      <h2>Solution with draggble boxes?</h2>
+      <Example2 />
+    </div>
+  </div>
+);
